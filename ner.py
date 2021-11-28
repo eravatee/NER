4890b5af5a7e79bd49477a04faa9a863603bafee
Eravatee Raje <eraraje1997@gmail.com>
2021-11-28 16:05:39 -0700

    Added changes for tagList and wordList

--- ner.py
+++ ner.py
@@ -3,8 +3,9 @@ from sklearn.model_selection import train_test_split
 
 def getSentences(trainingData):
     numberCheck = "(^[0-9])"
-    newLine = []
-    sentenceArray=[]
+    newLine = list()
+    sentenceArray = list()
+
     for line in trainingData:
         check = re.search(numberCheck, line)
         line.strip()
@@ -17,9 +18,25 @@ def getSentences(trainingData):
             newLine = []
     return sentenceArray
 
+def getPOSandWordLists(trainData):
+    wordList = list()
+    tagList = list()
+    for sentence in trainData:
+        for word, tag in sentence:
+            wordList.append(word)
+            tagList.append(tag)
+    return wordList, tagList
+
+def generateBigrams(wordList):
+    return zip(wordList, wordList[1:])
+# def tagTransitionProbabilities(testData):
+#     return
+
 InputFileName = "S21-gene-train.txt"
 trainingData = open(InputFileName, 'r').readlines()
 sentenceArray = getSentences(trainingData)
-train, test= train_test_split(sentenceArray, test_size=0.2)
-print("sentence: ", sentenceArray)
-
+traindata, testData = train_test_split(sentenceArray, test_size=0.2)
+wordList, tagList = getPOSandWordLists(traindata)
+bigrams = generateBigrams(wordList)
+print(bigrams)
+# print("sentence: ", sentenceArray)
